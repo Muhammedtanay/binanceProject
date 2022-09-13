abb8a144774d87d033ba3248d2652744dbaa18e7
MuhammedTanay <muhammedtny@gmail.com>
2022-09-13 11:33:34 +0300

    updated

--- Currencyto.cs
+++ Currencyto.cs
@@ -1,11 +1,11 @@
 
-Pinging youtube.com [172.217.169.206] with 32 bytes of data:
-Reply from 172.217.169.206: bytes=32 time=37ms TTL=114
-Reply from 172.217.169.206: bytes=32 time=37ms TTL=114
-Reply from 172.217.169.206: bytes=32 time=37ms TTL=114
-Reply from 172.217.169.206: bytes=32 time=37ms TTL=114
+Pinging gmail.com [172.217.17.133] with 32 bytes of data:
+Reply from 172.217.17.133: bytes=32 time=38ms TTL=115
+Reply from 172.217.17.133: bytes=32 time=38ms TTL=115
+Reply from 172.217.17.133: bytes=32 time=38ms TTL=115
+Reply from 172.217.17.133: bytes=32 time=38ms TTL=115
 
-Ping statistics for 172.217.169.206:
+Ping statistics for 172.217.17.133:
     Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
-    Minimum = 37ms, Maximum = 37ms, Average = 37ms
+    Minimum = 38ms, Maximum = 38ms, Average = 38ms
