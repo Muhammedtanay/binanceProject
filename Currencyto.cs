078772813c3bc996e03101e7c8cb4d9fb40c237b
MuhammedTanay <muhammedtny@gmail.com>
2022-09-13 15:22:32 +0300

    updated

--- Currencyto.cs
+++ Currencyto.cs
@@ -1,11 +1,11 @@
 
 Pinging google.com [172.217.169.142] with 32 bytes of data:
-Request timed out.
 Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
-Reply from 172.217.169.142: bytes=32 time=37ms TTL=55
-Reply from 172.217.169.142: bytes=32 time=37ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
 
 Ping statistics for 172.217.169.142:
-    Packets: Sent = 4, Received = 3, Lost = 1 (25% loss),
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
-    Minimum = 36ms, Maximum = 37ms, Average = 36ms
+    Minimum = 36ms, Maximum = 36ms, Average = 36ms
