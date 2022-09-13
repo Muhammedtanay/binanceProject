f172cecfef7d0d5d22c2f66ec25cce086cf784eb
MuhammedTanay <muhammedtny@gmail.com>
2022-09-13 18:17:20 +0300

    updated

--- Currencyto.cs
+++ Currencyto.cs
@@ -2,10 +2,10 @@
 Pinging google.com [172.217.17.110] with 32 bytes of data:
 Reply from 172.217.17.110: bytes=32 time=37ms TTL=56
 Reply from 172.217.17.110: bytes=32 time=37ms TTL=56
-Reply from 172.217.17.110: bytes=32 time=37ms TTL=56
-Reply from 172.217.17.110: bytes=32 time=39ms TTL=56
+Reply from 172.217.17.110: bytes=32 time=38ms TTL=56
+Reply from 172.217.17.110: bytes=32 time=38ms TTL=56
 
 Ping statistics for 172.217.17.110:
     Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
-    Minimum = 37ms, Maximum = 39ms, Average = 37ms
+    Minimum = 37ms, Maximum = 38ms, Average = 37ms
