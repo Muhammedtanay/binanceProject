99fd418222f7c1ad7667df3f88e1aa821e9bc5a9
MuhammedTanay <muhammedtny@gmail.com>
2022-09-13 11:25:50 +0300

    updated

--- Currencyto.cs
+++ Currencyto.cs
@@ -1,11 +1,11 @@
 
 Pinging google.com [142.250.187.110] with 32 bytes of data:
-Request timed out.
+Reply from 142.250.187.110: bytes=32 time=40ms TTL=54
 Reply from 142.250.187.110: bytes=32 time=38ms TTL=54
 Reply from 142.250.187.110: bytes=32 time=38ms TTL=54
-Reply from 142.250.187.110: bytes=32 time=37ms TTL=54
+Reply from 142.250.187.110: bytes=32 time=38ms TTL=54
 
 Ping statistics for 142.250.187.110:
-    Packets: Sent = 4, Received = 3, Lost = 1 (25% loss),
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
-    Minimum = 37ms, Maximum = 38ms, Average = 37ms
+    Minimum = 38ms, Maximum = 40ms, Average = 38ms
