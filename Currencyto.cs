81b5a5fb7fe9127a9197d39eb4674344aa4b1469
MuhammedTanay <muhammedtny@gmail.com>
2022-09-13 10:10:33 +0300

    updated

---
+++ Currencyto.cs
@@ -0,0 +1,11 @@
+
+Pinging google.com [172.217.169.142] with 32 bytes of data:
+Reply from 172.217.169.142: bytes=32 time=38ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=38ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
+Reply from 172.217.169.142: bytes=32 time=36ms TTL=55
+
+Ping statistics for 172.217.169.142:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 36ms, Maximum = 38ms, Average = 37ms
